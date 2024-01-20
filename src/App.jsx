00d79c61c5a23d62a8ac4a02cc37bca34abddee3
Emmanostrings-Bob <oluwafemiayomide852@gmail.com>
2024-01-20 15:36:56 +0100

    Testing mapTiler

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Map from './components/Map/Map';
+import Navbar from './components/Navbar/Navbar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Map />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className="heading">
+    <h1>This is my map App</h1>
+    </div>
+  )
+}
+
+export default Navbar
